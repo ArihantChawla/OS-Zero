a404ed8d59cf40d84da0f578c18fc7c6ba7c9d9e
Tuomo Venäläinen <tuomoster@gmail.com>
2013-01-11 02:53:50 +0200

    renamed byteswap macros

--- usr/lib/c/byteswap.h
+++ usr/lib/c/byteswap.h
@@ -5,16 +5,16 @@
  * NOTE: on the ia-32 architecture (P3), optimizing these with assembly didn't
  * seem worth it...
  */
-#define bswap16(x)  _bswap16(x)
-#define bswap32(x)  _bswap32(x)
-#define bswap64(x)  _bswap64(x)
+#define bswap_16(x)  _bswap_16(x)
+#define bswap_32(x)  _bswap_32(x)
+#define bswap_64(x)  _bswap_64(x)
 
-#define _bswap16(x) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8))
-#define _bswap32(x) ((((x) >> 24) & 0xff) \
+#define _bswap_16(x) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8))
+#define _bswap_32(x) ((((x) >> 24) & 0xff) \
                      | (((x) >> 8) & 0xff00) \
                      | (((x) & 0xff00) << 8) \
                      | (((x) & 0xff) << 24))
-#define _bswap64(x) ((((x) >> 56) & 0xff) \
+#define _bswap_64(x) ((((x) >> 56) & 0xff) \
                      | (((x) >> 40) & 0xff00) \
                      | (((x) >> 24) & 0x00ff0000) \
                      | (((x) >> 8) & 0xff000000) \
