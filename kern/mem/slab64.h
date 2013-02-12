e92affdac2555bf85b22f54c1015bba09bd4e1dc
Tuomo Venäläinen <tuomoster@gmail.com>
2013-02-12 13:35:55 +0200

    v-tree not necessary for allocator :)

--- kern/mem/slab64.h
+++ kern/mem/slab64.h
@@ -22,45 +22,5 @@
 #define slabsetnext(hp, hdr, tab)                                       \
     ((hp)->next = (hdr))
 
-#if 0
-#define SLABNLVLBIT  16
-#define SLABNLVL0BIT (PTRBITS - 3 * SLABNLVLBIT)
-
-static __inline__ void *
-slabgethdr(void *adr, void *tab)
-{
-    void *ptr = tab;
-    unsigned long mask = (1UL << NLVLBIT) - 1;
-#if (SLABNLVL0BIT)
-    unsigned long k0 = ((uintptr_t)adr >> (3 * SLABNLVLBIT))
-        & (1UL << (SLABNLVL0BIT - 1));
-#endif
-    unsigned long k1 = ((uintptr_t)adr >> (2 * SLABNLVLBIT)) & mask;
-    unsigned long k2 = ((uintptr_t)adr >> SLABLVLBIT) & mask;
-    unsigned long k3 = (uintptr_t)adr & mask;
-    
-#if (SLABNLVL0BIT)
-    ptr = (void **)ptr[k0];
-    if (!ptr) {
-        
-        return ptr;
-    }
-#endif
-    ptr = (void **)ptr[k1];
-    if (!ptr) {
-
-        return ptr;
-    }
-    ptr = (void **)ptr[k2];
-    if (!ptr) {
-
-        return ptr;
-    }
-    ptr = (void **)ptr[k3];
-
-    return ptr;
-}
-#endif
-
 #endif /* __MEM_SLAB64_H__ */
 
