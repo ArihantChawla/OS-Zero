ea2c886c1c648b1f34d14fabcb0175df1c7273e2
Tuomo Venäläinen <tuomoster@gmail.com>
2013-08-02 00:33:06 +0300

    some fixes for the event interface

--- kern/ev.h
+++ kern/ev.h
@@ -5,29 +5,31 @@
 #include <wchar.h>
 #include <zero/cdecl.h>
 
-typedef uint32_t evid_t;
-
 /* event header structure */
 
 /* user input events */
 
-/* event types */
+/* keyboard events */
 #define EVKEYDOWN       0x01
 #define EVKEYUP         0x02
+/* pointer events */
 #define EVBUTTONDOWN    0x03
 #define EVBUTTONUP      0x04
 #define EVPNTMOTION     0x05
+/* IPC events */
 #define EVCMD           0x06
-#define EVDATA          0x07
+#define EVMSG           0x07
+#define EVDATA          0x08
 /* filesystem events */
-#define EVFSCREAT       0x08
-#define EVFSUNLINK      0x09
-#define EVFSMKDIR       0x0a
-#define EVFSRMDIR       0x0b
+#define EVFSCREAT       0x09
+#define EVFSUNLINK      0x0a
+#define EVFSMKDIR       0x0b
+#define EVFSRMDIR       0x0c
 
 /* keyboard events */
 
-/* Unicode specifies 0x10ffff as maximum value, leaving us with 11 high bits to
+/*
+ * Unicode specifies 0x10ffff as maximum value, leaving us with 11 high bits to
  * be used as flags if need be
  */
 
@@ -67,44 +69,48 @@ struct evpnt {
     uint32_t z;                         // screen Z coordinate
 } PACK();
 
-struct uievent {
-    uint32_t type;                      // event type
-    union {                             // event data
-        struct evkbd kbd;
-        struct evpnt pnt;
-    } ev;
-} PACK();
-
 /* IPC events */
 
 /* command packet */
 struct evcmd {
     uint64_t cmd;                       // RPC command
+    uint64_t src;                       // src object
     uint64_t dest;                      // destination object
 } PACK();
 
-/* data packet */
+/* message packet */
+struct evmsg {
+    uint32_t nbyte;                     // number of octets
+    uint8_t  data[EMPTY];               // data (place-holder)
+} PACK();
+
+/* header + data */
 struct evdata {
     uint32_t fmt;                       // data format
     uint32_t itemsz;                    // data-word size
-    uint32_t nitem;                     // number of words
-    uint8_t  data[EMPTY];               // data
-} PACK();
-
-struct ipcevent {
-    uint32_t type;
-    union {
-        struct evcmd  cmd;
-        struct evdata data;
-    } ev;
-} PACK();
+    uint64_t nitem;                     // number of items to follow
+    uint64_t obj;                       // data object ID
+};
 
 /* file system events */
 
-struct fsevent {
+struct evfs {
     uint32_t type;                      // event type
     uint32_t dev;                       // device ID
-    int64_t  node;                      // node (file, directory) ID
+    uint64_t node;                      // node (file, directory) ID
+    uint8_t  data[EMPTY];               // optional event data
+} PACK();
+
+struct zevent {
+    uint32_t type;
+    union {
+        struct evkbd  kbd;
+        struct evpnt  pnt;
+        struct evcmd  cmd;
+        struct evmsg  msg;
+        struct evdata data;
+        struct evfs   fs;
+    };
 } PACK();
 
 /* API */
@@ -114,10 +120,11 @@ void        evreg(long ev, long flg, void *qadr);
 /* check queue for event type ev; parameters in flg */
 /* check current queue; don't flush server connection */
 #define EVNOFLUSH       0x01
-/* fetch/remove event from queue */
-#define EVFETCH         0x02
-struct ev * evchk(long ev, long flg, void *qadr);
+/* remove event from queue */
+#define EVREMOVE        0x02
+long        evchk(struct evp *ev, long mask, void *qadr);
 /* read next event from queue */
+/* flg bits */
 /* leave copy of fetched event in the queue */
 #define EVNOREMOVE      0x01
 /* do not flush server connection before get */
--- kern/unit/ia32/mkcdimg.sh
+++ kern/unit/ia32/mkcdimg.sh
@@ -5,7 +5,8 @@
 #genisoimage -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o cd.iso cdimg/
 # use GRUB 2
 cp zero cdimg2/kern
-echo "grub-mkrescue is broken"
-exit 1
+#grub-mkrescue -o cd.iso cdimg2/
+#echo "grub-mkrescue is broken"
+#exit 1
 
-grub-mkimage --prefix=/boot --config=cdimg2/boot/grub/grub.cfg --output=cd.iso --format=i386-pc biosdisk iso9660 multiboot
+#grub-mkimage --prefix=/boot --config=cdimg2/boot/grub/grub.cfg --output=cd.tmp --format=i386-pc biosdisk iso9660 multiboot
