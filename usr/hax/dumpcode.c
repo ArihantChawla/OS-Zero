d35abbf3d07a72767c0c09e836728fc775550e96
Tuomo Venäläinen <tuomoster@gmail.com>
2013-09-19 01:14:09 +0300

    improved usr/hax/dumpcode.c output

--- usr/hax/dumpcode.c
+++ usr/hax/dumpcode.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define DUMPCODE_NPERLINE 24
+
 void dumpcode(void *base, void *lim);
 
 extern void _start(void);
@@ -19,12 +21,19 @@ void
 dumpcode(void *base, void *lim)
 {
     uint8_t *ptr = base;
+    int      x = 0;
 
     if (ptr < (uint8_t *)lim) {
         printf("%2x", *ptr);
         ptr++;
         while (ptr < (uint8_t *)lim) {
-            printf(" %02x", *ptr);
+            x++;
+            if (x != DUMPCODE_NPERLINE) {
+                printf(" %02x", *ptr);
+            } else {
+                printf("\n%02x");
+                x = 0;
+            }
             ptr++;
         }
         printf("\n");
