801e4245d9c8782873df2bb5a0867e6e21c25d36
Tuomo Venäläinen <tuomoster@gmail.com>
2013-09-18 21:01:22 +0300

    simplified dumpcode

--- usr/hax/dumpcode.c
+++ usr/hax/dumpcode.c
@@ -4,8 +4,6 @@
 
 void dumpcode(void *base, void *lim);
 
-#define TEXT_WIDTH 80
-
 extern void _start(void);
 extern char _etext;
 
@@ -21,19 +19,10 @@ void
 dumpcode(void *base, void *lim)
 {
     uint8_t *ptr = base;
-    int      x = 0;
 
     while (ptr < (uint8_t *)lim) {
-        printf("%x", *ptr);
-        x++;
-        if (x == TEXT_WIDTH) {
-            printf("\n");
-            x = 0;
-        }
+        printf("%2x", *ptr);
         ptr++;
-    }
-    if (x) {
-        printf("\n");
     }
 
     return;
