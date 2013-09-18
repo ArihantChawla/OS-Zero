beb0713be78b1b76eac7d416aca83ae09dd98f73
Tuomo Venäläinen <tuomoster@gmail.com>
2013-09-19 01:31:34 +0300

    improved usr/hax/dumpcode.c output

--- usr/hax/dumpcode.c
+++ usr/hax/dumpcode.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define DUMPCODE_NPERLINE 16
+#define DUMPHEX_NPERLINE 16
 
-void dumpcode(void *base, void *lim);
+void dumphex(void *base, void *lim);
 
 extern void _start(void);
 extern char _etext;
@@ -12,29 +12,40 @@ extern char _etext;
 int
 main(int argc, char *argv[])
 {
-    dumpcode(_start, &_etext);
+    dumphex(_start, &_etext);
 
     exit(0);
 }
 
 void
-dumpcode(void *base, void *lim)
+dumphex(void *base, void *lim)
 {
     uint8_t *ptr = base;
+    uint8_t *line;
     int      x = 0;
 
     if (ptr < (uint8_t *)lim) {
+        line = ptr;
         printf("%2x", *ptr);
         ptr++;
         while (ptr < (uint8_t *)lim) {
             x++;
-            if (x != DUMPCODE_NPERLINE) {
+            if (x != DUMPHEX_NPERLINE) {
                 if (!(x & 0x03)) {
-                    printf("    %02x", *ptr);
+                    printf("  %02x", *ptr);
                 } else {
-                    printf(" %02x", *ptr);
+                    printf("%02x", *ptr);
                 }
             } else {
+                printf("    ");
+                while (line < ptr) {
+                    if (isprint(*line)) {
+                        printf(" %c", *line);
+                    } else {
+                        printf("%02x", *line);
+                    }
+                    line++;
+                }
                 printf("\n%02x");
                 x = 0;
             }
