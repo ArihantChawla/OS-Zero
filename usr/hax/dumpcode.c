40f669c77f27f95d5b4f0127e6fa29a08b4dd5b6
Tuomo Venäläinen <tuomoster@gmail.com>
2013-09-18 20:40:06 +0300

    added usr/hax =)

---
+++ usr/hax/dumpcode.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void dumpcode(void *base, void *lim);
+
+#define TEXT_WIDTH 80
+
+extern char _etext;
+
+int
+main(int argc, char *argv[])
+{
+    dumpcode((void *)&main, &_etext);
+
+    exit(0);
+}
+
+void
+dumpcode(void *base, void *lim)
+{
+    uint8_t *ptr = base;
+    int      x = 0;
+
+    while (ptr < (uint8_t *)lim) {
+        printf("%x", *ptr);
+        x++;
+        if (x == TEXT_WIDTH) {
+            printf("\n");
+            x = 0;
+        }
+        ptr++;
+    }
+    if (x) {
+        printf("\n");
+    }
+
+    return;
+}
+
