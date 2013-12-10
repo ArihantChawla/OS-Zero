16c3d1406973f318fee9c50335360b1034227d1d
Tuomo Venäläinen <tuomoster@gmail.com>
2013-12-10 10:04:14 +0200

    added Zeno\'s new 24-bit plasma hacks

--- usr/contrib/Zeno/build.sh
+++ usr/contrib/Zeno/build.sh
@@ -4,6 +4,7 @@ CC=gcc
 
 $CC -I../../lib -DPROF=1 -O -Wall -o plasma plasma.c -lm -lSDL
 $CC -I../../lib -DPROF=1 -g -O -Wall -o plasma24 plasma24.c -lm -lSDL
+$CC -I../../lib -DPROF=1 -g -O -Wall -o plasma24grid plasma24grid.c -lm -lSDL
 #$CC -I../../lib -DTEST=1 -DZPLASMA=1 -O -Wall -o zplasma zplasma.c -lm -lSDL
 #$CC -I../../lib -DPROF=1 -O -Wall -o fire firebasic.c -lm -lSDL
 #$CC -I../../lib -DPROF=1 -O -Wall -o lava lava.c -lm -lSDL
