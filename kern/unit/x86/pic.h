fbbc07d7cfb1ac3eebe33b3edd3436d7a435f24e
Tuomo Venäläinen <tuomoster@gmail.com>
2014-08-24 22:38:51 +0300

    readability and bug fixes etc. :)

--- kern/unit/x86/pic.h
+++ kern/unit/x86/pic.h
@@ -1,11 +1,15 @@
 #ifndef __UNIT_X86_PIC_H__
 #define __UNIT_X86_PIC_H__
 
+/* PIC registers */
 #define PICINIT  0x11
 #define PIC1     0x20
 #define PICMASK1 0x21
 #define PIC2     0xa0
 #define PICMASK2 0xa1
+
+/* PIC commands */
+#define PICEOI   0x20
 
 #endif /* __UNIT_X86_PIC_H__ */
 
