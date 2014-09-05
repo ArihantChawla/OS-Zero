1bbd84721b9707e7dd67265a9432abf13edc72c2
Tuomo Venäläinen <tuomoster@gmail.com>
2014-09-05 22:04:06 +0300

    ...

--- usr/lib/c/stddef.h
+++ usr/lib/c/stddef.h
@@ -1,6 +1,7 @@
 #ifndef __STDDEF_H__
 #define __STDDEF_H__
 
+#include <features.h>
 #include <stdint.h>
 
 #define NULL           ((void *)0L)
@@ -8,8 +9,10 @@
 typedef long           ptrdiff_t;
 typedef unsigned long  size_t;
 /* full ISO10646 character set aka Unicode */
+#if (_ZERO_SOURCE) || (__STDC_ISO_10646__)
 typedef int32_t        wchar_t;
 typedef int32_t        wint_t;
+#endif
 
 #define offsetof(t, m) ((size_t)(&((t *)0)->m))
 
