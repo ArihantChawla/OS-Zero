ea46810c7deaf574de2fd77fbd9e4409c397791d
Tuomo Venäläinen <tuomoster@gmail.com>
2014-09-05 20:48:00 +0300

    small libc hacks

--- usr/lib/c/stddef.h
+++ usr/lib/c/stddef.h
@@ -3,11 +3,13 @@
 
 #include <stdint.h>
 
-#define NULL ((void *)0x00000000L)
+#define NULL           ((void *)0L)
 
-typedef long          ptrdiff_t;
-typedef unsigned long size_t;
-typedef int32_t       wchar_t;
+typedef long           ptrdiff_t;
+typedef unsigned long  size_t;
+/* full ISO10646 character set aka Unicode */
+typedef int32_t        wchar_t;
+typedef int32_t        wint_t;
 
 #define offsetof(t, m) ((size_t)(&((t *)0)->m))
 
--- usr/lib/c/wchar.h
+++ usr/lib/c/wchar.h
@@ -1,14 +1,24 @@
 #ifndef __WCHAR_H__
 #define __WCHAR_H__
 
-typedef int32_t wchar_t;
+#include <stddef.h>
 
-#define WEOF        0xffff
-#define WCUCMAX     0x10ffff
+#define WCHAR_MIN   0
+#define WCHAR_MAX   0xffffffff
+#define WEOF        ((wint_t)0xffff)	/* 0xffff is invalid character value in Unicode */
+#define WCUCMAX     0x0010ffff
+#define WCUCMASK    0x001fffff
 #define WCNUCBIT    21
+#if 0
 #define WCUCFLGMASK 0xffe00000
 #define WCFLGMASK   0x7ff
 #define WCNFLGBIT   11
+#endif /* 0 */
+/* use high 8 bits for character flag-bits; low 24 bits for character value */
+#define WCUCFLGMASK 0xff000000
+#define WCFLGMASK   0x000000ff
+#define WCNFLGBIT   8
+#define WCFLGSHIFT  (32 - WCNFLGBIT)
 
 #endif /* __WCHAR_H__ */
 
