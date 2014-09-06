267391564b3750bf0a6909ebe03f32fc0945852f
Tuomo Venäläinen <tuomoster@gmail.com>
2014-09-06 17:31:02 +0300

    added missing file

---
+++ kern/io.h
@@ -0,0 +1,24 @@
+#ifndef __KERN_IO_H__
+#define __KERN_IO_H__
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#define SEEK_CUR        0x00
+#define SEEK_SET        0x01
+#define SEEK_END        0x02
+
+struct file {
+    off_t      ofs;
+    uintptr_t  desc;
+    char      *path;
+    long       flg;
+    long       errflg;
+    
+};
+
+/* TODO: implement iogetc() */
+#define iogetc(fp)
+
+#endif /* __KERN_IO_H__ */
+
