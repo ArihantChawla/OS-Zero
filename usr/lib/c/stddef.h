8b8d3ae8259ff49d3884f7b56ff6178a921281da
Tuomo Venäläinen <tuomoster@gmail.com>
2014-09-06 10:20:57 +0300

    small changes here and there

--- usr/lib/c/stddef.h
+++ usr/lib/c/stddef.h
@@ -1,18 +1,28 @@
 #ifndef __STDDEF_H__
 #define __STDDEF_H__
 
-#include <features.h>
+#include <features.h>v
 #include <stdint.h>
 
 #define NULL           ((void *)0L)
 
 typedef long           ptrdiff_t;
 typedef unsigned long  size_t;
-/* full ISO10646 character set aka Unicode */
-#if (_ZERO_SOURCE) || (__STDC_ISO_10646__)
-typedef int32_t        wchar_t;
-typedef int32_t        wint_t;
+#if (__STDC_ISO_10646__)
+/* full ISO10646 character */
+#define WCHARSIZE      4
+#else
+/* 16-bit Unicode character */
+#define WCHARSIZE      2
 #endif
+#if (WCHARSIZE == 4)
+typedef int32_t        wchar_t;
+#elif (WCHARSIZE == 2)
+typedef uint16_t       wchar_t;
+#elif (WCHARSIZE == 1)
+typedef uint8_t        wchar_t;
+#endif
+typedef int            wint_t;
 
 #define offsetof(t, m) ((size_t)(&((t *)0)->m))
 
--- usr/lib/c/wchar.h
+++ usr/lib/c/wchar.h
@@ -3,22 +3,25 @@
 
 #include <stddef.h>
 
-#define WCHAR_MIN   0
-#define WCHAR_MAX   0xffffffff
-#define WEOF        ((wint_t)0xffff)	/* 0xffff is invalid character value in Unicode */
-#define WCUCMAX     0x0010ffff
-#define WCUCMASK    0x001fffff
-#define WCNUCBIT    21
-#if 0
-#define WCUCFLGMASK 0xffe00000
-#define WCFLGMASK   0x7ff
-#define WCNFLGBIT   11
-#endif /* 0 */
-/* use high 8 bits for character flag-bits; low 24 bits for character value */
-#define WCUCFLGMASK 0xff000000
-#define WCFLGMASK   0x000000ff
-#define WCNFLGBIT   8
-#define WCFLGSHIFT  (32 - WCNFLGBIT)
+#define WEOF         ((wint_t)0xffff)	/* 0xffff is invalid character value in Unicode */
+#define WCHAR_MIN    0
+#if (WCHARSIZE == 4)
+#define WCHAR_MAX    0xffffffff
+#define WCHARUCMAX   0x0010ffff
+#define WCHARMASK    0x001fffff
+#define WCHARNBIT    21
+#elif (WCHARSIZE == 2)
+#define WCHAR_MAX    0xffff
+#define WCHARUCMAX   0xffff
+#define WCHARMASK    0xffff
+#define WCHARNBIT    16
+#elif (WCHARSIZE == 1)
+#define WCHAR_MAX    0xff
+#define WCHARUCMAX   0xff
+#define WCHARMASK    0xff
+#define WCHARNBIT    8
+#endif /* WCHARSIZE */
+#define WCHARMASK    ((1 << WCHARNBIT) - 1)
 
 #endif /* __WCHAR_H__ */
 
