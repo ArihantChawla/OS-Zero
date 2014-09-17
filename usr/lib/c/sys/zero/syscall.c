f0b31aa847af96a8d248a7cfc42becfbdcfc8782
Tuomo Venäläinen <tuomoster@gmail.com>
2014-09-18 01:30:02 +0300

    made _syscall deal with errno

--- usr/lib/c/sys/zero/syscall.c
+++ usr/lib/c/sys/zero/syscall.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <zero/param.h>
 #include <zero/cdecl.h>
 
@@ -10,13 +11,18 @@ _syscall(long num, long arg1, long arg2, long arg3)
     long retval;
 
     retval = -1;
-    __asm__ __volatile__ ("movq %1, %%eax\n"
-                          "movq %2, %%ebx\n"
-                          "movq %3, %%ecx\n"
-                          "movq %4, %%edx\n"
+    __asm__ __volatile__ ("movq %2, %%eax\n"
+                          "movq %3, %%ebx\n"
+                          "movq %4, %%ecx\n"
+                          "movq %5, %%edx\n"
                           "int $0x80\n"
+                          "jc 1\n"
                           "movq %%eax, %0\n"
-                          : "=a" (retval)
+                          "jmp 2\n"
+                          "1:\n"
+                          "movq %%eax, %1\n"
+                          "2:\n"
+                          : "=a" (retval), "=m" (errno)
                           : "rm" (num), "rm" (arg1), "rm" (arg2), "rm" (arg3));
 
     return retval;
@@ -31,13 +37,18 @@ _syscall(long num, long arg1, long arg2, long arg3)
     long retval;
 
     retval = -1;
-    __asm__ __volatile__ ("movq %1, %%rax\n"
-                          "movq %2, %%rbx\n"
-                          "movq %3, %%rcx\n"
-                          "movq %4, %%rdx\n"
+    __asm__ __volatile__ ("movq %2, %%rax\n"
+                          "movq %3, %%rbx\n"
+                          "movq %4, %%rcx\n"
+                          "movq %5, %%rdx\n"
                           "int $0x80\n"
+                          "jc 1\n"
                           "movq %%rax, %0\n"
-                          : "=a" (retval)
+                          "jmp 2\n"
+                          "1:\n"
+                          "movq %%rax, %1\n"
+                          "2:\n"
+                          : "=a" (retval), "=m" (errno)
                           : "rm" (num), "rm" (arg1), "rm" (arg2), "rm" (arg3));
 
     return retval;
