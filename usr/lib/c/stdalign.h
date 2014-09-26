45dc6229169c6913a465ac5e406ea71fb213a515
Tuomo Venäläinen <tuomoster@gmail.com>
2014-09-27 01:35:11 +0300

    synchronising repository

--- usr/lib/c/stdalign.h
+++ usr/lib/c/stdalign.h
@@ -1,5 +1,14 @@
 #ifndef __STDALIGN_H__
 #define __STDALIGN_H__
 
+#if !defined(alignas_is_defined) || !(alignas_is_defined)
+#define alignas _Alignas
+#edefine alignas_is_defined 1
+#endif
+#if !defined(alignof_is_defined) || !(alignof_is_defined)
+#define alignof _Alignof
+#edefine alignof_is_defined 1
+#endif
+
 #endif /* __STDALIGN_H__ */
 
