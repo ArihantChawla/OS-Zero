e4e1d9660f3203a92637834f700c5f2e2fb4e604
Tuomo Venäläinen <tuomoster@gmail.com>
2014-11-28 03:17:03 +0200

    added a couple of missing files

---
+++ usr/lib/c/sched.h
@@ -0,0 +1,7 @@
+#ifndef __SCHED_H__
+#define __SCHED_H__
+
+int sched_yield(void);
+
+#endif /* __SCHED_H__ */
+
---
+++ usr/lib/c/unistd.c
@@ -0,0 +1,11 @@
+#include <zero/cdecl.h>
+#include <zero/param.h>
+
+int
+getpagesize(void)
+{
+    int retval = PAGESIZE;
+
+    return retval;
+}
+
