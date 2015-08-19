e62349cc7f5e22c57e300ac33641e6d85065a5f3
Tuomo Venäläinen <tuomoster@gmail.com>
2015-08-19 04:12:10 +0300

    added errno.c

--- usr/lib/c/sys/zero/errno.c
+++ usr/lib/c/sys/zero/errno.c
@@ -1,8 +1,9 @@
-#include <errno.h>
-
-#if (PTHREAD)
-__thread int errno;
+#if defined(__STDC_VERSION__) && (__STDC_VERSION >= 201112L)            \
+    && !defined(__STDC_NO_THREADS__)
+thread_local int errno;
+#elif (PTHREAD) && defined(__GNUC__)
+__thread int     errno;
 #else
-int          errno;
+int              errno;
 #endif
 
