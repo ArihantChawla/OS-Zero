78837b48cd4153572350aafcdd5d59695aa78465
Tuomo Venäläinen <tuomoster@gmail.com>
2015-09-18 20:45:37 +0300

    reorganising + fixing stuff

---
+++ usr/lib/zero/macros.h
@@ -0,0 +1,13 @@
+#ifndef __ZERO_MACROS_H__
+#define __ZERO_MACROS_H__
+
+#if defined(__GCC__)
+#define containerof(ptr, type, member)                                  \
+    ({                                                                  \
+        const typeof(((type *)0)->member) *_mptr = (ptr);               \
+        (type *)((char *)_mptr - offsetof(type, member));               \
+    })
+#endif /* __GCC__ */
+
+#endif /* __ZERO_MACROS_H__ */
+
