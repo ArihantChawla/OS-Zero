ef9c86c25c582aafeba6caf72d22b8dd5798bb1f
Tuomo Venäläinen <tuomoster@gmail.com>
2015-12-20 04:19:11 +0200

    more hacks

--- usr/lib/c/execinfo.c
+++ usr/lib/c/execinfo.c
@@ -7,26 +7,139 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <dlfcn.h>
+#include <execinfo.h>
 #include <zero/asm.h>
 
-#define _BTPRINTFMT  "%a <%n%D> at %f\n"
+#define _BTPRINTFMT  "%a <%n%D> at %f"
 #define _BTOFSSTRLEN 32
-#define _BTSTRLEN    128
+#define _BTTABLEN    4096
 
 uintptr_t
-_btprint(void **buf, int size, const char *fmt, int fd)
+_btprintf(void **buf, int size, const char *fmt, int fd)
 {
-    unsigned int   lim = size;
-    char         **tab;
-    unsigned int   ndx;
+    Dl_info       info;
+    FILE         *fp = (fd >= 0) ? fdopen(fd, "a") : NULL;
+    unsigned int  lim = size;
+    int           slen = 0;
+    char         *mptr = NULL;
+    size_t        len;
+    uintptr_t     pdif;
+    void         *ptr;
+    const char   *fptr;
+    char         *cptr;
+    char         *tab;
+    unsigned int  ndx;
+    long          nl;
 
+    if (!fp) {
+        len = _BTTABLEN;
+        mptr = malloc(len);
+        if (!mptr) {
+
+            return (uintptr_t)-1;
+        }
+    }
+    tab = mptr;
+    cptr = tab;
     if (!fmt) {
         fmt = _BTPRINTFMT;
     }
-    for (ndx = 0 ; ndx < size ; ndx++) {
-    }
+    for (ndx = 0 ; ndx < lim ; ndx++) {
+        nl = 0;
+        fptr = fmt;
+        ptr = buf[ndx];
+        dladdr(ptr, &info);
+        if (fp) {
+            while (fptr[0] != '\0') {
+                if (fptr[0] != '%') {
+                    fprintf(fp, "%c", fptr[0]);
+                    fptr++;
+                } else if (fptr[1] == 'a') {
+                    fprintf(fp, "%p", ptr);
+                    fptr += 2;
+                } else if (fptr[1] == 'n') {
+                    fprintf(fp, "%s", info.dli_sname);
+                    fptr += 2;
+                } else if (fptr[1] == 'd') {
+                    pdif = (uintptr_t)ptr - (uintptr_t)info.dli_saddr;
+                    fprintf(fp, "0x%lx", ptr, (long)pdif);
+                    fptr += 2;
+                } else if (fptr[1] == 'D') {
+                    pdif = (uintptr_t)ptr - (uintptr_t)info.dli_saddr;
+                    if (pdif) {
+                        fprintf(fp, "+0x%lx", (long)pdif);
+                    }
+                    fptr += 2;
+                } else if (fptr[1] == 'f') {
+                    fprintf(fp, "%s", info.dli_fname);
+                    fptr += 2;
+                }
+            }
+            if (fptr == '\0') {
+                fprintf(fp, "%c", '\n');
+                nl = 1;
 
-    return (uintptr_t)tab;
+                continue;
+            }
+        } else {
+            while (fptr[0] != '\0') {
+                if (fptr[0] != '%') {
+                    cptr[0] = fptr[0];
+                    cptr++;
+                    fptr++;
+                } else if (fptr[1] == 'a') {
+                    slen += snprintf(cptr, len - slen, "%p", ptr);
+                    fptr += 2;
+                } else if (fptr[1] == 'n') {
+                    slen += snprintf(cptr, len - slen, "%s", info.dli_sname);
+                    fptr += 2;
+                } else if (fptr[1] == 'd') {
+                    pdif = (uintptr_t)ptr - (uintptr_t)info.dli_saddr;
+                    slen += snprintf(cptr, len - slen, "0x%lx",
+                                     ptr, (long)pdif);
+                    fptr += 2;
+                } else if (fptr[1] == 'D') {
+                    pdif = (uintptr_t)ptr - (uintptr_t)info.dli_saddr;
+                    if (pdif) {
+                        slen += snprintf(cptr, len - slen, "+0x%lx",
+                                         (long)pdif);
+                    }
+                    fptr += 2;
+                } else if (fptr[1] == 'f') {
+                    slen += snprintf(cptr, len - slen, "%s", info.dli_fname);
+                    fptr += 2;
+                }
+                pdif = (uintptr_t)cptr - (uintptr_t)tab;
+                if (pdif >= (len >> 1)) {
+                    len <<= 1;
+                    mptr = realloc(tab, len);
+                    if (!mptr) {
+                        free(tab);
+
+                        return (uintptr_t)-1;
+                    }
+                    tab = mptr;
+                    cptr = mptr + slen;
+                }
+            }
+            if (fptr == '\0') {
+                cptr[0] = '\n';
+                nl = 1;
+                cptr++;
+
+                continue;
+            }
+        }
+    }
+    if (!nl) {
+        if (fp) {
+            fprintf(fp, "%c", '\n');
+        } else {
+            cptr[0] = '\n';
+        }
+    }
+    
+    return (uintptr_t)mptr;
 }
 
 uintptr_t
--- usr/lib/c/execinfo.h
+++ usr/lib/c/execinfo.h
@@ -5,16 +5,17 @@
 
 #if defined(_GNU_SOURCE)
 extern uintptr_t _backtrace(void **buf, int size, long syms, int fd);
-#define          backtrace(buf, sz)                                     \
-    (int)_backtrace(buf, sz, 0, -1)
-#define          backtrace_symbols(buf, sz)                             \
-    (char **)_backtrace(NULL, sz, 1, -1)
-#define          backtrace_symbols_fd(buf, sz, fd)                      \
-    (void)_backtrace(NULL, sz, 1, fd)
-#define          backtrace_symbols_fmt(buf, len, fmt)                   \
-    (char **)_btprint(buf, len, fmt, -1)
-#define          backtrace_symbols_fmt_fd(buf, len, fmt, fd)            \
-    (char **)_btprint(buf, len, fmt, fd)
+extern uintptr_t _btprintf(void **buf, int size, const char *fmt, int fd);
+#define          backtrace(buf, size)                                   \
+    (int)_backtrace(buf, size, 0, -1)
+#define          backtrace_symbols(buf, size)                           \
+    (char **)_backtrace(NULL, size, 1, -1)
+#define          backtrace_symbols_fd(buf, size, fd)                    \
+    (void)_backtrace(NULL, size, 1, fd)
+#define          backtrace_symbols_fmt(buf, size, fmt)                  \
+    (char **)_btprintf(buf, size, fmt, -1)
+#define          backtrace_symbols_fmt_fd(buf, size, fmt, fd)           \
+    (char **)_btprintf(buf, size, fmt, fd)
 #endif /* defined(_GNU_SOURCE) */
 
 #endif /* __EXECINFO_H__ */
