5b0f9b23c14c9172880cfba476b1320f8d34ae33
Tuomo Venäläinen <tuomoster@gmail.com>
2015-12-21 15:44:59 +0200

    added a couple of zpc files

---
+++ usr/tools/zpc/conf.h
@@ -0,0 +1,7 @@
+#ifndef __ZPC_CONF_H__
+#define __ZPC_CONF_H__
+
+#define ZPC32BIT 1
+
+#endif /* __ZPC_CONF_H__ */
+
---
+++ usr/tools/zpc/zpc32.h
@@ -0,0 +1,42 @@
+#ifndef __ZPC_ZPC32_H__
+#define __ZPC_ZPC32_H__
+
+#include <stdint.h>
+
+typedef int32_t zpcreg_t;
+typedef double  zpcfreg_t;
+
+/* OPCODE structure */
+
+/* # of registers */
+#define ZPCREGS          (1 << ZPCREGBITS)
+/* opcode bit-field widths */
+#define ZPCOPIDBITS      5
+#define ZPCOPFLAGBITS    3
+#define ZPCREGBITS       4
+#define ZPCOPIMMEDBITS   6
+/* flg-bits */
+#define ZPCOPLOCKBIT     (1 << 0)
+#define ZPCOPFPUBIT      (1 << 1)
+#define ZPCOPVECBIT      (1 << 2)
+/* adrmode-bits */
+#define ZPCARGIMMEDBIT   (1 << 0)
+#define ZPCARGREGBIT     (1 << 1)
+#define ZPCARGINDEXBIT   (1 << 2)
+#define ZPCARGINDIRBIT   (1 << 3)
+#define ZPCOPARGBIT      (1 << 4)
+#define ZPCOPADRMODEBITS 5
+#define zpcophasarg(op)  ((op)->argmode & ZPCOPARGBIT)
+struct zpcop {
+    unsigned id      : ZPCOPIDBITS;             // 5-bit
+    unsigned flg     : ZPCOPFLAGBITS;           // 3-bit
+    unsigned src     : ZPCREGBITS;              // 4-bit
+    unsigned dest    : ZPCREGBITS;              // 4-bit
+    unsigned adrmode : ZPCOPADRMODEBITS;        // 5-bit
+    unsigned adrreg  : ZPCREGBITS;              // 4-bit
+    unsigned immed   : ZPCOPIMMEDBITS;          // 7-bit
+    uint32_t arg[EMPTY];                        // optional argument
+};
+
+#endif /* #ifndef __ZPC_ZPC32_H__ */
+
