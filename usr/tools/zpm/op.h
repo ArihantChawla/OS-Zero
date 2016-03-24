b78c97f59e8dabac3ff8d9514c8b381545001689
Tuomo Venäläinen <tuomoster@gmail.com>
2016-03-25 01:04:37 +0200

    hacking around

--- usr/tools/zpm/op.h
+++ usr/tools/zpm/op.h
@@ -4,31 +4,27 @@
 #define zpmgetjmparg(vm, op)                                            \
     zpmgetsrcarg(vm, op)
 #define zpmgetsrcarg(vm, op)                                            \
-    (((op)->adr & ZPM_REG_VAL)                                          \
-     ? (((vm)->regs[(op)->reg1])                                        \
-        ? (((op)->adr & ZPM_IMM8_VAL)                                   \
-           ? ((op)->imm8)                                               \
-           : ((op)->args[0]))))
+    (((op)->adrmode & ZPM_REG_VAL)                                      \
+     ? ((vm)->regs[(op)->reg1])                                         \
+     : (((op)->adrmode & ZPM_IMM8_VAL)                                  \
+        ? ((op)->imm8)                                                  \
+        : ((op)->args[0])))
 #define zpmgetdestptr(vm, op)                                           \
-    (((op)->adr & ZPM_REG_VAL)                                          \
+    (((op)->adrmode & ZPM_REG_VAL)                                      \
      ? (&(vm)->regs[(op)->reg2])                                        \
      : (&(vm)->physmem[(op)->args[0]]))
 #define zpmgetldrsrcptr(vm, op)                                         \
-    (((op)->adr & ZPM_REG_ADR)                                          \
-     ? (&(vm)->physmem[(op)->reg1])                                     \
-     : (((op)->adr & ZPM_REG_NDX)                                       \
+    (((op)->adrmode & ZPM_REG_ADR)                                      \
+     ? (((op)->adrmode & ZPM_REG_NDX)                                   \
         ? (&(vm)->physmem[(op)->reg1 + (op)->args[0]])                  \
-        : (((op)->adr & ZPM_IMM_ADR)                                    \
-           ? (((op)->adr & ZPM_REG_NDX)                                 \
-              ? (&(vm)->physmem[(op)->reg1 + (op)->args[0]])            \
-              : (&(vm)->physmem[(op)->args[0]])))))
+        : (&(vm)->physmem[(op)->reg1]))                                 \
+     : (&(vm)->physmem[(op)->args[0]]))
 #define zpmgetstrdestptr(vm, op)                                        \
-    (((op)->adr & ZPM_REG_ADR)                                          \
-     ? (&(vm)->physmem[(op)->reg2])                                     \
-     : (((op)->adr & ZPM_IMM_ADR)                                       \
-        ? (((op)->adr & ZPM_REG_NDX)                                    \
-           ? (&(vm)->physmem[(op)->reg2 + (op)->args[0]])               \
-           : (&(vm)->physmem[(op)->args[0]]))))
+    (((op)->adrmode & ZPM_REG_ADR)                                      \
+     ? (((op)->adrmode & ZPM_REG_NDX)                                   \
+        ? (&(vm)->physmem[(op)->reg2 + (op)->args[0]])                  \
+        : (&(vm)->physmem[(op)->reg2]))                                 \
+     : (&(vm)->physmem[(op)->args[0]]))
 #define zpmsetzf(val)                                                   \
     (!(val) \
      ? (zpm.msw_reg |= ZPM_MSW_ZF) \
