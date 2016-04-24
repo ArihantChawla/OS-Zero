0831c35c34ac1d9a84e6fe8cca9f138aff1293f9
Tuomo Venäläinen <tuomoster@gmail.com>
2016-04-24 22:37:26 +0300

    modding v86

--- usr/games/v86/v86.h
+++ usr/games/v86/v86.h
@@ -19,52 +19,55 @@ typedef uint32_t v86adr;
 
 /* instruction set */
 
-#define V86_NOP              0x00
-#define V86_NOT              0x01
-#define V86_AND              0x02
-#define V86_OR               0x03
-#define V86_XOR              0x04
-#define V86_SHL              0x05
-#define V86_SHR              0x06
-#define V86_SAR              0x07
-#define V86_ADD              0x08
-#define V86_SUB              0x09
-#define V86_CMP              0x0a
-#define V86_MUL              0x0b
-#define V86_DIV              0x0c
-#define V86_JMP              0x0d
-#define V86_JE               0x0e
-#define V86_JNE              0x0f
-#define V86_JLT              0x10
-#define V86_JLE              0x11
-#define V86_JGT              0x12
-#define V86_JGE              0x13
-#define V86_JC               0x14
-#define V86_JNC              0x15
-#define V86_JO               0x16
-#define V86_JNO              0x17
-#define V86_CALL             0x18
-#define V86_RET              0x19
-#define V86_LDR              0x1a
-#define V86_STR              0x1b
-#define V86_PUSH             0x1c
-#define V86_PUSHA            0x1d
-#define V86_POP              0x1e
-#define V86_POPA             0x1f
-#define V86_IN               0x20
-#define V86_OUT              0x21
-#define V86_HLT              0x22
+#define V86_NOP              0x00       // no operation
+#define V86_NOT              0x01       // NOT r/i, r
+#define V86_AND              0x02       // AND r/i, r
+#define V86_OR               0x03       // OR r/i, r
+#define V86_XOR              0x04       // XOR r/i, r
+#define V86_SHL              0x05       // SHL r/i, r
+#define V86_SHR              0x06       // SHR r/i, r
+#define V86_SAR              0x07       // SAR r/i, r
+#define V86_ADD              0x08       // ADD r/i, r
+#define V86_SUB              0x09       // SUB r/i, r
+#define V86_CMP              0x0a       // CMP r/i, r - set flags
+#define V86_MUL              0x0b       // MUL r/i, r
+#define V86_DIV              0x0c       // DIV r/i, r
+#define V86_JMP              0x0d       // JMP r/m      - unconditional
+#define V86_JZ               0x0e       // JE r/m       - ZF == 1
+#define V86_JE               V86_JZ     // JZ r/m       - ZF == 1
+#define V86_JNZ              0x0f       // JNE r/m      - ZF == 0
+#define V86_JNE              V86_JNZ    // JNZ r/m      - ZF == 0
+#define V86_JC               0x10       // JC r/m       - CF == 1
+#define V86_JLT              V86_JC     // JLT r/m      - CF == 1
+#define V86_JNC              0x11       // JNC r/m      - CF == 0
+#define V86_JO               0x12       // JO r/m       - OF == 1
+#define V86_JNO              0x13       // JNO r/m      - OF == 0
+#define V86_JLE              0x14       // JLE r/m      - SF != OF || ZF == 1
+#define V86_JGT              0x15       // JGT r/m      - ZF == 0 || SF == OF
+#define V86_JGE              0x16       // JGE r/m      - SF == OF
+#define V86_CALL             0x17       // CALL r/m
+#define V86_RET              0x18       // RET i
+#define V86_LDR              0x19       // LDR i/m, r
+#define V86_STR              0x1a       // STR r, i/m
+#define V86_PUSH             0x1b       // PUSH r/i
+#define V86_PUSHA            0x1c       // PUSHA m
+#define V86_POP              0x1d       // POP r
+#define V86_POPA             0x1e       // POPA
+#define V86_IN               0x1f       // IN r/i, r
+#define V86_OUT              0x20       // OUT r, r/i
+#define V86_HLT              0x21       // HLT
 /* codes through 0x3f reserved for future use */
+#define V86_OPERATIONS       0x22
 #define V86_MAX_OPERATIONS   64         // maximum # of operations supported
 
 /* operand type flags; 0 for register */
-#define V86_CONSTANT_OPERAND (1 << 0)   // constant
+#define V86_IMMEDIATE_OPERAND (1 << 0)  // immediate
 /* addressing mode flags */
 #define V86_DIRECT_ADDRESS   (1 << 1)   // direct addressing; ldr adr, %r
 #define V86_INDIRECT_ADDRESS (1 << 2)   // indirect addressing; ldr *%ar, %r
 #define V86_INDEXED_ADDRESS  (1 << 3)   // indexed addressing; ldr ofs(%ar), %r
-#define V86_IMMEDIATE_MASK                                              \
-    (V86_CONSTANT_OPERAND | V86_INDIRECT_ADDRESS | V86_INDEXED_ADDRESS)
+#define V86_ARGUMENT_MASK                                               \
+    (V86_IMMEDIATE_OPERAND | V86_INDIRECT_ADDRESS | V86_INDEXED_ADDRESS)
 
 /* user-accessible register IDs */
 #define V86_AX_REGISTER      0x00       // general purpose register AX
@@ -87,84 +90,8 @@ struct v86op {
     unsigned int sreg   : 2;    // source register ID
     unsigned int dreg   : 2;    // destination register ID
     unsigned int imm    : 16;   // 16-bit immediate offset or constant
-    uint32_t     arg[EMPTY];    // possible 32-bit constant, address, or offset
+    int32_t      arg[EMPTY];    // possible 32-bit constant, address, or offset
 };
-
-/* INSTRUCTION TIMINGS (prof) */
-
-/* REFERENCE: http://www.agner.org/optimize/instruction_tables.pdf */
-/* instruction timings in cycles (as listed for pentium [mmx] in REFERENCE */
-
-/*
- * MEMORY
- * ------
- * - cacheline fetch: 4 + CLSIZE clock cycles (size on 32-bit words)
- * - instruction fetch: 1 clock cycle
- * - instruction decode: 1 clock cycle
- * - immediate operand fetch: 2 clock cycles
- * - memory read/write: 4 clock cycles
- *
- */
-/* NOP
- * ---
- * - 1 clock cycle
- */
-/* NOT
- * ---
- * - 2 clock cycles
- */
-/* AND, OR, XOR
- * ------------
- * - 2 clock cycles+ operands
- */
-/* SHR, SHL, SAR, ROR, ROL
- * -----------------------
- * - 3 clock cycles + 1 for immediate operand
- */
-/* INC, DEC
- * --------
- * - 2 clock cycles
- */
-/* ADD, SUB
- * --------
- * - 2 clock cycles if both are registers
- * - 3 clock cycles with immediate operand
- */
-/* CMP
- * ---
- * - 2 clock cycles if both are registers
- * - 3 clock cycles with immediate operand
- */
-/* MUL
- * ---
- * - 8 clock cycles
- */
-/* DIV
- * ---
- * - 32 clock cycles
- */
-/* LDA, STA
- * --------
- * - src and dest are registers: 2
- * - src/dest is immediate operand: 3
- * - src/dest is a memory operand: 4
- */
-/* PUSH, POP
- * ---------
- * - 4 cycles if src/dest is a register
- */
-/* PUSHA, POPA
- * - 4 cycles + 1 cycle per register
- */
-/*
- * JMP, branches
- * -------------
- * - 8 clock cycles for the jump
- */
-/*
- * IN, OUT
- * - 8 clock cycles
- */
 
 #endif /* __V86_V86_H__ */
 
