5bd18d87f4103c35a9ec813d89a140f3f0579dd9
Tuomo Venäläinen <tuomoster@gmail.com>
2016-04-28 01:04:15 +0300

    minor fixes for v86

--- usr/games/v86/rat.h
+++ usr/games/v86/rat.h
@@ -19,53 +19,62 @@
 #define M_32BIT
 #endif
 
+#define v86rat64getsign(rp) ((rp)->num & 0x80000000)
 struct v86rat64 {
-    int32_t num;
-    int32_t den;
+    uint32_t num;
+    uint32_t den;
 };
 
 #if defined(M_64BIT)
 
+#define v86rat128getsign(rp) ((rp)->num[0] & UINT64_C(0x8000000000000000))
 struct v86rat128 {
-    int64_t num[2];
-    int64_t den[2];
+    uint64_t num[1];
+    uint64_t den[1];
 };
 
+#define v86rat256getsign(rp) ((rp)->num[1] & UINT64_C(0x8000000000000000))
 struct v86rat256 {
-    int64_t num[4];
-    int64_t den[4];
+    uint64_t num[2];
+    uint64_t den[2];
 };
 
+#define v86rat512getsign(rp) ((rp)->num[3] & UINT64_C(0x8000000000000000))
 struct v86rat512 {
-    int64_t num[8];
-    int64_t den[8];
+    uint64_t num[4];
+    uint64_t den[4];
 };
 
+#define v86rat512getsign(rp) ((rp)->num[7] & UINT64_C(0x8000000000000000))
 struct v86rat1024 {
-    int64_t num[16];
-    int64_t den[16];
+    uint64_t num[8];
+    uint64_t den[8];
 };
 
 #elif defined(M_32BIT)
 
+#define v86rat128getsign(rp) ((rp)->num[1] & 0x80000000)
 struct v86rat128 {
-    int32_t num[4];
-    int32_t den[4];
+    uint32_t num[2];
+    uint32_t den[2];
 };
 
+#define v86rat256getsign(rp) ((rp)->num[3] & 0x80000000)
 struct v86rat256 {
-    int32_t num[8];
-    int32_t den[8];
+    uint32_t num[4];
+    uint32_t den[4];
 };
 
+#define v86rat512getsign(rp) ((rp)->num[7] & 0x80000000)
 struct v86rat512 {
-    int32_t num[16];
-    int32_t den[16];
+    uint32_t num[8];
+    uint32_t den[8];
 };
 
+#define v86rat128getsign(rp) ((rp)->num[15] & 0x80000000)
 struct v86rat1024 {
-    int32_t num[32];
-    int32_t den[32];
+    uint32_t num[16];
+    uint32_t den[16];
 };
 
 #endif /* M_64BIT || M_32BIT */
