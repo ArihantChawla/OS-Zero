e602da974ae92e36415caacad085c05fb748e38a
Tuomo Venäläinen <tuomoster@gmail.com>
2016-06-25 17:31:20 +0300

    hacking on v86

--- usr/games/v86/op.h
+++ usr/games/v86/op.h
@@ -15,45 +15,266 @@ struct v86vmop {
     v86opfunc *func;
 };
 
-void v86opnot(struct v86vm *vm, struct v86op *op);
-void v86opand(struct v86vm *vm, struct v86op *op);
-void v86opor(struct v86vm *vm, struct v86op *op);
-void v86opxor(struct v86vm *vm, struct v86op *op);
-void v86opshl(struct v86vm *vm, struct v86op *op);
-void v86opshr(struct v86vm *vm, struct v86op *op);
-void v86opsar(struct v86vm *vm, struct v86op *op);
-void v86opadd(struct v86vm *vm, struct v86op *op);
-void v86opsub(struct v86vm *vm, struct v86op *op);
-void v86opcmp(struct v86vm *vm, struct v86op *op);
-void v86opmul(struct v86vm *vm, struct v86op *op);
-void v86opdiv(struct v86vm *vm, struct v86op *op);
-void v86opjmp(struct v86vm *vm, struct v86op *op);
-void v86opljmp(struct v86vm *vm, struct v86op *op);
-void v86opjz(struct v86vm *vm, struct v86op *op);
-void v86opjnz(struct v86vm *vm, struct v86op *op);
-void v86opjc(struct v86vm *vm, struct v86op *op);
-void v86opjnc(struct v86vm *vm, struct v86op *op);
-void v86opjo(struct v86vm *vm, struct v86op *op);
-void v86opjno(struct v86vm *vm, struct v86op *op);
-void v86opjle(struct v86vm *vm, struct v86op *op);
-void v86opjgt(struct v86vm *vm, struct v86op *op);
-void v86opjge(struct v86vm *vm, struct v86op *op);
-void v86opcall(struct v86vm *vm, struct v86op *op);
-void v86opret(struct v86vm *vm, struct v86op *op);
-void v86opldr(struct v86vm *vm, struct v86op *op);
-void v86opstr(struct v86vm *vm, struct v86op *op);
-void v86oppush(struct v86vm *vm, struct v86op *op);
-void v86oppusha(struct v86vm *vm, struct v86op *op);
-void v86oppop(struct v86vm *vm, struct v86op *op);
-void v86oppopa(struct v86vm *vm, struct v86op *op);
-void v86opin(struct v86vm *vm, struct v86op *op);
-void v86opout(struct v86vm *vm, struct v86op *op);
-void v86ophlt(struct v86vm *vm, struct v86op *op);
+/* NOT r/a, r */
+#define _v86opnot(vm, op, ret)                                          \
+    do {                                                                \
+        _v86getargs(vm, op);                                            \
+        dest = ~src;                                                    \
+        pc += sizeof(uint32_t);                                         \
+        vmsetureg(dptr, dest);                                          \
+        ret = dest;                                                     \
+        vmsetpc(pc);                                                    \
+    } while (0)
+
+/* FIXME: addressing modes below */
+
+/* NOT r, r */
+#define _v86opand(vm, op, ret)                                          \
+    do {                                                                \
+        _v86getargs(vm, op);                                            \
+        dest &= src;                                                    \
+        pc += sizeof(uint32_t);                                         \
+        vmsetureg(dptr, dest);                                          \
+        ret = dest;                                                     \
+        vmsetpc(pc);                                                    \
+    } while (0)
+ 
+/* OR r, r */
+#define _v86opor(vm, op, ret)                                           \
+    do {                                                                \
+        _v86getargs(vm, op);                                            \
+        dest |= src;                                                    \
+        pc += sizeof(uint32_t);                                         \
+        vmsetureg(dptr, dest);                                          \
+        ret = dest;                                                     \
+        vmsetpc(pc);                                                    \
+    } while (0)
+
+/* XOR r, r */
+#define _v86opxor(vm, op, ret)                                          \
+    do {                                                                \
+        _v86getargs(vm, op);                                            \
+        dest ^= src;                                                    \
+        pc += sizeof(uint32_t);                                         \
+        vmsetureg(dptr, dest);                                          \
+        ret = dest;                                                     \
+        vmsetpc(pc);                                                    \
+    } while (0)
+
+/* SHL i, r */
+#define _v86opshl(vm, op, ret)                                          \
+    do {                                                                \
+        v86reg res;                                                     \
+                                                                        \
+        _v86getargs(vm, op);                                            \
+        res = dest << src;                                              \
+        pc += sizeof(uint32_t);                                         \
+        vmsetureg(dptr, res);                                           \
+        ret = res;                                                      \
+        vmsetpc(pc);                                                    \
+    } while (0)
+
+/* SHR i, r */
+#define _v86opshr(vm, op, ret)                                          \
+    do {                                                                \
+        v86reg res;                                                     \
+                                                                        \
+        ret = res;                                                      \
+    } while (0)
+
+/* SAR i, r */
+#define _v86opsar(vm, op, ret)                                          \
+    do {                                                                \
+        v86reg res;                                                     \
+                                                                        \
+        ret = res;                                                      \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opadd(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opsub(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opcmp(vm, op, ret)                                          \
+    do {                                                                \
+        v86reg res;                                                     \
+                                                                        \
+        ret = res;                                                      \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opmul(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opdiv(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjmp(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opljmp(vm, op, ret)                                         \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjz(vm, op, ret)                                           \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjnz(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjc(vm, op, ret)                                           \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjnc(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjo(vm, op, ret)                                           \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjno(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjle(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjgt(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opjge(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opcall(vm, op, ret)                                         \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opret(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opldr(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opstr(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86oppush(vm, op, ret)                                         \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86oppusha(vm, op, ret)                                        \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86oppop(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86oppopa(vm, op, ret)                                         \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opin(vm, op, ret)                                           \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86opout(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
+
+/* NOT r/a, r */
+#define _v86ophlt(vm, op, ret)                                          \
+    do {                                                                \
+        ret = dest;                                                     \
+    } while (0)
 
 #if (V86_WORD_SIZE == 4)
 #define v86chkof(res) ((res) & ~(v86ureg)UINT32_MAX)
 #define v86chkuf(res) ((res) < (v86ureg)INT32_MIN)
 #endif
+
+#define _v86getargs(vm, op)                                             \
+    v86reg *dptr = NULL;                                                \
+    v86reg  dest = (((op)->opflg & V86_DESTINATION_REGISTER)            \
+                    ? (dptr = &(vm)->usrregs[(op)->dreg], *dptr)        \
+                    : (v86reg)((op)->arg));                             \
+    v86reg  src = (((op)->opflg & V86_IMMEDIATE_OPERAND)                \
+                   ? (v86reg)((op)->imm8)                               \
+                   : (((op)->opflg & V86_SOURCE_REGISTER)               \
+                      ? (v86reg)((vm)->usrregs[(op)->sreg])             \
+                      : (v86reg)((op)->arg)));                          \
+    v86reg  pc = (vm)->sysregs[V86_PC_REGISTER]
+
+#define vmsetpc(val)                                                    \
+    ((vm)->sysregs[V86_PC_REGISTER] = (val))
+#define vmsetureg(ptr, val)                                             \
+    (*ptr = (val))
 
 #endif /* __V86_OP_H__ */
 
--- usr/games/v86/vm.c
+++ usr/games/v86/vm.c
@@ -1,4 +1,204 @@
+#include <stddef.h>
 #include <v86/v86.h>
+#include <v86/vm.h>
+
+static __inline__ long
+vmdov86op(struct v86vm *vm, struct v86op *op)
+{
+    long    code = 0;
+    long    retval = -1L;
+    v86reg *dptr = NULL;
+    v86reg  dest;
+    v86reg  src;
+    v86reg  pc;
+
+    
+    do {
+        if (code) {
+          __v86not:
+            _v86opnot(vm, op, retval);
+            
+            return retval;
+          __v86and:
+            _v86opand(vm, op, retval);
+            
+            return retval;
+          __v86or:
+            _v86opor(vm, op, retval);
+            
+            return retval;
+          __v86xor:
+            _v86opxor(vm, op, retval);
+            
+            return retval;
+          __v86shl:
+            _v86opshl(vm, op, retval);
+
+            return retval;
+          __v86shr:
+            _v86opshr(vm, op, retval);
+            
+            return retval;
+          __v86sar:
+            _v86opsar(vm, op, retval);
+            
+            return retval;
+          __v86add:
+            _v86opadd(vm, op, retval);
+            
+            return retval;
+          __v86sub:
+            _v86opsub(vm, op, retval);
+            
+            return retval;
+          __v86cmp:
+            _v86opcmp(vm, op, retval);
+            
+            return retval;
+          __v86mul:
+            _v86opmul(vm, op, retval);
+            
+            return retval;
+          __v86div:
+            _v86opdiv(vm, op, retval);
+            
+            return retval;
+          __v86jmp:
+            _v86opjmp(vm, op, retval);
+            
+            return retval;
+          __v86ljmp:
+            _v86opljmp(vm, op, retval);
+
+            return retval;
+          __v86jz:
+            _v86opjz(vm, op, retval);
+            
+            return retval;
+          __v86jnz:
+            _v86opjnz(vm, op, retval);
+            
+            return retval;
+          __v86jc:
+            _v86opjc(vm, op, retval);
+            
+            return retval;
+          __v86jnc:
+            _v86opjnc(vm, op, retval);
+
+            return retval;
+          __v86jo:
+            _v86opjo(vm, op, retval);
+            
+            return retval;
+          __v86jno:
+            _v86opjno(vm, op, retval);
+            
+            return retval;
+          __v86jle:
+            _v86opjle(vm, op, retval);
+            
+            return retval;
+          __v86jgt:
+            _v86opjgt(vm, op, retval);
+            
+            return retval;
+          __v86jge:
+            _v86opjge(vm, op, retval);
+            
+            return retval;
+          __v86call:
+            _v86opcall(vm, op, retval);
+
+            return retval;
+          __v86ret:
+            _v86opret(vm, op, retval);
+
+            return retval;
+          __v86ldr:
+            _v86opldr(vm, op, retval);
+
+            return retval;
+          __v86str:
+            _v86opstr(vm, op, retval);
+
+            return retval;
+          __v86push:
+            _v86oppush(vm, op, retval);
+
+            return retval;
+          __v86pusha:
+            _v86oppusha(vm, op, retval);
+
+            return retval;
+          __v86pop:
+            _v86oppop(vm, op, retval);
+
+            return retval;
+          __v86popa:
+            _v86oppopa(vm, op, retval);
+
+            return retval;
+          __v86in:
+            _v86opin(vm, op, retval);
+
+            return retval;
+          __v86out:
+            _v86opnout(vm, op, retval);
+
+            return retval;
+          __v86hlt:
+            _v86ophlt(vm, op, retval);
+
+            return retval;
+        }
+        do {
+            void *jmptab[V86_OPERATIONS] =
+            {
+                NULL,
+                &&__v86not,
+                &&__v86and,
+                &&__v86or,
+                &&__v86xor,
+                &&__v86shl,
+                &&__v86shr,
+                &&__v86sar,
+                &&__v86add,
+                &&__v86sub,
+                &&__v86cmp,
+                &&__v86mul,
+                &&__v86div,
+                &&__v86jmp,
+                &&__v86ljmp,
+                &&__v86jz,
+                &&__v86jnz,
+                &&__v86jc,
+                &&__v86jnc,
+                &&__v86jo,
+                &&__v86jno,
+                &&__v86jle,
+                &&__v86jgt,
+                &&__v86jge,
+                &&__v86call,
+                &&__v86ret,
+                &&__v86ldr,
+                &&__v86str,
+                &&__v86push,
+                &&__v86pusha,
+                &&__v86pop,
+                &&__v86popa,
+                &&__v86in,
+                &&__v86out,
+                &&__v86hlt
+            };
+            code = op->code;
+
+            goto *jmptab[code];
+        } while (code);
+    } while (0);
+    
+    return retval;
+}
 
 int
 main(int argc, char *argv[])
