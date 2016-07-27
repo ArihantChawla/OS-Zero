6a2b893141ebf9b7a523b41d6909243ad35c815a
Tuomo Venäläinen <tuomoster@gmail.com>
2016-07-27 21:57:26 +0300

    minor fixes

--- usr/lib/zero/x86-64/bignum.h
+++ usr/lib/zero/x86-64/bignum.h
@@ -7,5 +7,35 @@ typedef __uint128_t u128;
 u128 addu128(u128 a, u128 b);
 u128 mulu128(u128 a, u128 b);
 
+static __inline__ i128
+add128(i128 a, i128 b) {
+
+    return a + b;
+}
+
+static __inline__ i128
+sub128(i128 a, i128 b) {
+
+    return a - b;
+}
+
+static __inline__ i128
+mul128(i128 a, i128 b) {
+
+    return a * b;
+}
+
+static __inline__ i128
+div128(i128 a, i128 b) {
+
+    return a / b;
+}
+
+static __inline__ i128
+rem128(i128 a, i128 b) {
+
+    return a % b;
+}
+
 #endif /* __ZERO_X86_64_BIGNUM_H__ */
 
