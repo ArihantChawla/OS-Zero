ab6d0675c2e0a6ba76e762901b04f49005860f0e
Tuomo Venäläinen <tuomoster@gmail.com>
2016-07-27 22:16:58 +0300

    minor fixes

--- usr/lib/zero/x86-64/tagptr.h
+++ usr/lib/zero/x86-64/tagptr.h
@@ -13,12 +13,16 @@
                  (volatile long *)want,                                 \
                  (volatile long *)src)
 
+/* tp = { 0, adr } */
 #define tagptrinitadr(adr, tp)                                          \
     ((tp) = _mm_cvtsi64_si128((long long)adr), (tp) = _mm_slli_si128((tp), 64))
+/* dest.tag = src.tag */
 #define tagptrcpytag(src, dest)                                         \
     ((dest) = _mm_or_si128(dest, _mm_loadl_epi64(&src)))
+/* tp = { tp.tag++, tp.adr } */
 #define tagptrinctag(tp)                                                \
     (_mm_add_ss((__m128)tp, (__m128)_mm_cvtsi64_si128(INT64_C(1))))
+/* tag.adr */
 #define tagptrgetadr(tp)                                                \
     ((void *)((uintptr_t)_mm_cvtsi128_si64(_mm_srli_si128(tp, 64))))
 
