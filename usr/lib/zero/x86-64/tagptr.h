ee85d5a4a8a1ea1fb0874fa11b54e82d960a2fd0
Tuomo Venäläinen <tuomoster@gmail.com>
2016-07-28 18:54:09 +0300

    fixes for tagptr.h

--- usr/lib/zero/x86-64/tagptr.h
+++ usr/lib/zero/x86-64/tagptr.h
@@ -18,13 +18,6 @@
 #define tagptrinitadr(adr, tp)                                          \
     ((tp) = _mm_cvtsi64_si128((long long)adr), (tp) = _mm_slli_si128((tp), 64))
 /* dest.tag = src.tag */
-#if 0
-#define tagptrcpytag(src, dest)                                         \
-    ((dest) = _mm_or_si128(dest, _mm_loadl_epi64(&src)))
-/* tp = { tp.tag++, tp.adr } */
-#define tagptrinctag(tp)                                                \
-    (_mm_add_ss((__m128)tp, (__m128)_mm_cvtsi64_si128(INT64_C(1))))
-#endif
 #define tagptrgettag(tp)                                                \
     _mm_cvtsi128_si64(tp)
 #define tagptrsettag(tag, tp)                                           \
