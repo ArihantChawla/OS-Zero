9f746a9dd650b7a782a357757970681725980829
Tuomo Venäläinen <tuomoster@gmail.com>
2016-07-28 18:18:41 +0300

    fixes for tagged pointers / lock-free queue

--- usr/lib/zero/lfq.c
+++ usr/lib/zero/lfq.c
@@ -4,18 +4,20 @@
 #include <zero/lfq.h>
 
 void
-lfqqueue(struct lfq *q, struct lfqnode *node)
+lfqenqueue(struct lfq *q, struct lfqnode *node)
 {
-    TAGPTR_T tail;
-    TAGPTR_T tmp;
-    TAGPTR_T src;
+    TAGPTR_T    tail;
+    TAGPTR_T    tmp;
+    TAGPTR_T    src;
+    TAGPTRTAG_T tag;
 
     do {
         tagptrinitadr(node, src);               // src.adr = node
         tail = q->tail;
-        tagptrcpytag(tail, src);                // src.tag = tail.tag
-        tmp = src;
-        tagptrinctag(tmp);                      // tmp.tag = tail.tag + 1
+        tag = tagptrgettag(tail);               // tag = tail.tag
+        tagptrsettag(tag, src);                 // src.tag = tail.tag
+        tag++;                                  // tag = tail.tag + 1;
+        tagptrsettag(tag, tmp);                 // tmp.tag = tail.tag + 1
         node->next = tmp;
         if (tagptrcmpswap(&q->tail, &tail, &tmp)) {
             ((struct lfqnode *)tagptrgetadr(tail))->prev = src;
--- usr/lib/zero/x86-64/tagptr.h
+++ usr/lib/zero/x86-64/tagptr.h
@@ -6,7 +6,8 @@
 #include <zero/asm.h>
 
 /* pointer in the high-order 64 bits, tag [counter] in the low */
-#define TAGPTR_T __m128i
+#define TAGPTR_T    __m128i
+#define TAGPTRTAG_T uint64_t
 
 #define tagptrcmpswap(tp, want, src)                                    \
     m_cmpswapdbl((volatile long *)tp,                                   \
@@ -17,11 +18,17 @@
 #define tagptrinitadr(adr, tp)                                          \
     ((tp) = _mm_cvtsi64_si128((long long)adr), (tp) = _mm_slli_si128((tp), 64))
 /* dest.tag = src.tag */
+#if 0
 #define tagptrcpytag(src, dest)                                         \
     ((dest) = _mm_or_si128(dest, _mm_loadl_epi64(&src)))
 /* tp = { tp.tag++, tp.adr } */
 #define tagptrinctag(tp)                                                \
     (_mm_add_ss((__m128)tp, (__m128)_mm_cvtsi64_si128(INT64_C(1))))
+#endif
+#define tagptrgettag(tp)                                                \
+    _mm_cvtsi128_si64(tp)
+#define tagptrsettag(tag, tp)                                           \
+    ((tp) = _mm_or_si128(tp, _mm_cvtsi64_si128(tag)))
 /* tag.adr */
 #define tagptrgetadr(tp)                                                \
     ((void *)((uintptr_t)_mm_cvtsi128_si64(_mm_srli_si128(tp, 64))))
