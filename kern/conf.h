48873fcc34ad3346af0189622332b0820ead50d2
Tuomo Venäläinen <tuomoster@gmail.com>
2016-08-19 09:59:52 +0300

    kernel runs again, to a point =)

--- kern/conf.h
+++ kern/conf.h
@@ -30,7 +30,7 @@
 
 #define kgethz()    HZ
 #define HZ          250         // scheduler timer frequency
-#define SMP         1           // enable multiprocessor support; FIXME:  broken
+#define SMP         0           // enable multiprocessor support; FIXME:  broken
 #define SMT         0           // enable hardware multithreading ('hyper')
 #define FASTHZ      1000        // fast timer frequency (interactive tasks etc.)
 #define ZEROSCHED   1           // default scheduler
