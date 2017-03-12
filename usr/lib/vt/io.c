0ffc78a4f37e4cf837b78b8a73101c9d447e1a43
Tuomo Venäläinen <tuomoster@gmail.com>
2017-03-12 15:11:58 +0200

    small fixes in libvt

--- usr/lib/vt/io.c
+++ usr/lib/vt/io.c
@@ -3,21 +3,25 @@
 #include <stdlib.h>
 #include <vt/term.h>
 #endif
-#include <vt/vt.h>
+#if defined(__KERNEL__) && (__KERNEL__)
+#define FREE(x)   kfree(x)
+#define MALLOC(x) kmalloc(x)
+#else
 #if !defined(FREE)
 #define FREE(x) free(x)
 #endif
 #if !defined(MALLOC)
 #define MALLOC(x) malloc(x)
 #endif
+#endif
+#include <vt/vt.h>
 
 #if defined(__KERNEL__) && (__KERNEL__)
 
 long
 vtinitbuf(struct vt *vt)
 {
-    if (!ringinit(&vt->devbuf.in, NULL, VTBUFSIZE / sizeof(RING_ITEM))
-        || !ringinit(&vt->devbuf.in, NULL, VTBUFSIZE / sizeof(RING_ITEM))) {
+    if (!ringinit(&vt->devbuf.in, NULL, VTBUFSIZE / sizeof(RING_ITEM))) {
         
         return 0;
     }
@@ -32,12 +36,12 @@ vtfreebuf(struct vt *vt)
 
     ptr = vt->devbuf.in.base;
     if ((ptr) && ptr != vt->devbuf.in.data) {
-        free(ptr);
+        FREE(ptr);
         vt->devbuf.in.base = NULL;
     }
     ptr = vt->devbuf.out.base;
     if ((ptr) && ptr != vt->devbuf.out.data) {
-        free(ptr);
+        FREE(ptr);
         vt->devbuf.out.base = NULL;
     }
 }
