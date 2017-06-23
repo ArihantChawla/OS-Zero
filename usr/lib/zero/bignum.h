b909f62d612b2209019d8c0f0131bde05fac406a
Tuomo Venäläinen <tuomoster@gmail.com>
2017-06-24 01:07:58 +0300

    hacking <zero/bignum.h>

--- usr/lib/zero/bignum.h
+++ usr/lib/zero/bignum.h
@@ -14,5 +14,44 @@
 #include <zero/ppc/bignum.h>
 #endif
 
+#if defined(__USE_BIGNUM128) && (__USE_BIGNUM128)
+
+u128 addu128(u128 a, u128 b);
+u128 mulu128(u128 a, u128 b);
+
+static __inline__ i128
+add128(i128 a, i128 b) {
+
+    return a + b;
+}
+
+static __inline__ i128
+sub128(i128 a, i128 b) {
+
+    return a - b;
+}
+
+static __inline__ i128
+mul128(i128 a, i128 b) {
+
+    return a * b;
+}
+
+static __inline__ i128
+div128(i128 a, i128 b) {
+
+    return a / b;
+}
+
+#define mod128(a, b) rem128(a, b)
+
+static __inline__ i128
+rem128(i128 a, i128 b) {
+
+    return a % b;
+}
+
+#endif
+
 #endif /* __ZERO_BIGNUM_H__ */
 
--- usr/lib/zero/x86-64/bignum.h
+++ usr/lib/zero/x86-64/bignum.h
@@ -1,13 +1,9 @@
 #ifndef __ZERO_X86_64_BIGNUM_H__
 #define __ZERO_X86_64_BIGNUM_H__
 
-#if defined(__GNUC__)
-#include <quadmath.h>
-typedef __float128        f128;
-#else
-/* let's try */
-typedef Quad              f128;
-#endif
+#define __USE_BIGNUM128 1
+
+/* determine proper 128-bit scalar type */
 #if defined(__GNUC__)
 typedef __int128          i128;
 typedef unsigned __int128 u128;
@@ -15,41 +11,14 @@ typedef unsigned __int128 u128;
 typedef  __int128_t       i128
 typedef  __uint128_t      u128
 #endif
-
-u128 addu128(u128 a, u128 b);
-u128 mulu128(u128 a, u128 b);
-
-static __inline__ i128
-add128(i128 a, i128 b) {
-
-    return a + b;
-}
-
-static __inline__ i128
-sub128(i128 a, i128 b) {
-
-    return a - b;
-}
-
-static __inline__ i128
-mul128(i128 a, i128 b) {
-
-    return a * b;
-}
-
-static __inline__ i128
-div128(i128 a, i128 b) {
-
-    return a / b;
-}
-
-#define mod128(a, b) rem128(a, b)
-
-static __inline__ i128
-rem128(i128 a, i128 b) {
-
-    return a % b;
-}
+/* determine proper 128-bit floating-point type */
+#if defined(__GNUC__)
+#include <quadmath.h>
+typedef __float128        f128;
+#else
+/* let's try */
+typedef Quad              f128;
+#endif
 
 #endif /* __ZERO_X86_64_BIGNUM_H__ */
 
