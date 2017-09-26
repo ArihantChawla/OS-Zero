cf74fb5592717f77b6f0156cc18ca6e3ffedc935
Tuomo Venäläinen <tuomoster@gmail.com>
2017-09-26 12:38:15 +0300

    extending V0 documentation

--- usr/tools/valhalla/vpu/v0/cc/reg.c
+++ usr/tools/valhalla/vpu/v0/cc/reg.c
@@ -1,9 +1,16 @@
 /* valhalla c compiler register allocation/management */
 
+#define VCC_MAX_REGS 256
+
+struct vccmemreg {
+    void            *adr;
+    union vccregval  val;
+};
+
 struct vccreg {
-    zerofmtx_t       lk;  // mutual exclusion lock
-    unsigned long    id;  // register ID
-    union vccregval  val; // cached register contents
+    zerofmtx_t       lk;   // mutual exclusion lock
+    union vccregval  val;  // cached register contents
+    struct vccreg   *prev; // pointer to previous in queue
     struct vccreg   *next; // pointer to next in queue;
 };
 
@@ -19,14 +26,7 @@ struct vccreg {
  *   - set register backup location to ptr, then call vccfreereg()
  */
 struct vccregmgr {
-    struct vccreg      usedq; // queue of registers in use
-    struct vccreg      freeq; // list of registers deallocated explicitly
+    struct vccreg *used; // queue of registers in use
+    struct vccreg *free; // list of registers deallocated explicitly
 };
-
-#define VCC_MAX_REGS 256
-
-struct vccreg vccgenregs[VCC_MAX_REGS];
-#if 0
-struct vccfreg vccfltregs[VCC_MAX_REGS];
-#endif
 
