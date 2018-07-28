baed6e30c0d8ace64ea463e75c2ca13a564daf9e
Tuomo Venäläinen <tuomoster@gmail.com>
2018-07-28 09:17:10 +0300

    improving/fixing array hash table

--- usr/lib/zero/tabhash.h
+++ usr/lib/zero/tabhash.h
@@ -6,23 +6,30 @@
 #include <mach/asm.h>
 #include <zero/trix.h>
 
+/*
+ * array hash, chains tables of items instead of single ones to avoid excess
+ * pointer data dependencies/chase
+ */
+
 /* allocation hash-table queue structure; cache-friendly */
 struct tabhashhdr {
     m_atomic_t     n;
     volatile void *chain;
 };
 
+/* allocation hash data entry */
 #if !defined(TABHASH_ITEM_T)
 #define TABHASH_ITEM_T struct tabhashitem
 #endif
-/* allocation hash entry */
 struct tabhashitem {
     uintptr_t key;
     uintptr_t val;
 };
 
-//struct tabhashbuf;
-
+/*
+ * allocation chain table buffer; should consume a few aligned cachelines and
+ * is loaded in a few fetches only to avoid cache-trashing
+ */
 #define TABHASH_TAB_ITEMS      31
 #define TABHASH_BUF_LK_BIT     (1L << TABHASH_BUF_LK_BIT_POS)
 #define TABHASH_BUF_LK_BIT_POS 0
@@ -31,6 +38,9 @@ struct tabhashbuf {
     TABHASH_ITEM_T    tab[TABHASH_TAB_ITEMS];
 };
 
+/*
+ * acquire chain/buffer lock by locking the lowest bit of a pointer
+ */
 static __inline__ void
 tabhashlkbuf(struct tabhashbuf **buf)
 {
@@ -48,6 +58,9 @@ tabhashlkbuf(struct tabhashbuf **buf)
     return NULL;
 }
 
+/*
+ * release lock/low bit of a pointer
+ */
 static __inline__ void
 tabhashunlkbuf(struct tabhashbuf **buf)
 {
@@ -57,12 +70,18 @@ tabhashunlkbuf(struct tabhashbuf **buf)
     return;
 }
 
+/* convenience macros */
+/* allocate new hash chain table */
 #define tabhashalloctab() calloc(1, sizeof(struct tabhashbuf))
+/* find hash entry, return pointer but do not remove it */
+#define tabhashfind(tab, key) tabhashsearch(tab, key, TAB_HASH_SEARCH)
+/* find and remove a hash entry */
+#define tabhashdel(tab, key)  tabhashsearch(tab, key, TAB_HASH_REMOVE)
 
 #define TABHASH_REMOVE -1
 #define TABHASH_SEARCH  0
 static __inline__ uintptr_t
-tabhashop(struct tabhashbuf **hash, uintptr_t key, long op)
+tabhashsearch(struct tabhashbuf **hash, uintptr_t key, long op)
 {
     unsigned long      hkey = TABHASH_FUNC(key);
     uintptr_t          val;
@@ -85,8 +104,8 @@ tabhashop(struct tabhashbuf **hash, uintptr_t key, long op)
                 ni = min(n, 8);
                 if (ni) {
                     /*
-                     * - if item found, the mask will be -1; all 1-bits), and val
-                     *   will be the item address
+                     * - if item found, the mask will be -1; all 1-bits),
+                     *   and val will be the item address
                      * - if not found, the mask will be 0 and so will val
                      */
                     val = 0;
@@ -139,11 +158,11 @@ tabhashop(struct tabhashbuf **hash, uintptr_t key, long op)
                     if (val) {
                         item = (TABHASH_ITEM_T *)val;
                         if (op == TABHASH_REMOVE) {
-                            src = buf->hdr.n;
                             n--;
+                            src = n;
                             ndx = item - &buf->tab[0];
                             if (!n) {
-                                /* TODO: deallocate table */
+                                /* TODO: deallocate table or keep it */
                             } else if (ndx != src) {
                                 buf->tab[ndx] = buf->tab[src];
                                 buf->tab[src].key = key;
@@ -153,6 +172,9 @@ tabhashop(struct tabhashbuf **hash, uintptr_t key, long op)
                                 buf->tab[ndx].val = TABHASH_VAL_NONE;
                             }
                             buf->hdr.n = n;
+                            /*
+                             * TODO: implement add to front caching of items
+                             */
                         }
                         tabhashunlkbuf(&hash[hkey]);
 
@@ -171,7 +193,4 @@ tabhashop(struct tabhashbuf **hash, uintptr_t key, long op)
 
     return val;
 }
-
-#define tabhashfind(tab, key) tabhashchk(tab, key, 0)
-#define tabhashdel(tab, key)  tabhashchk(tab, key, 1)
 
